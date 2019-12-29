889e7f603223aabab6f8ffad61ee5be273ab45df
Tim Johnsen <timonus@gmail.com>
2019-12-28 19:40:14 -0700

    Add test project.

---
+++ Test Project/UIImageViewAnimatedGIFTestBed/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  UIImageViewAnimatedGIFTestBed
+//
+//  Created by Tim Johnsen on 12/28/19.
+//  Copyright © 2019 Tim Johnsen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ Test Project/UIImageViewAnimatedGIFTestBed/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  UIImageViewAnimatedGIFTestBed
+//
+//  Created by Tim Johnsen on 12/28/19.
+//  Copyright © 2019 Tim Johnsen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
